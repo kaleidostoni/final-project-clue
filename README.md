bc80ef00a5380757d817c28f673b8e37719f1b5c
kaleidostoni <kaleidostoni@gmail.com>
2017-10-27 12:31:20 +0000

    subiendo la carpeta

---
+++ README.md
@@ -0,0 +1,5 @@
+##Pistas sobre proyecto final
+1.repaso sobre metodoso
+2.metodos para los strings
+3.ascii
+4.for, condicionales
---
+++ app.js
@@ -0,0 +1,31 @@
+var testArray = [];
+for(var i = 0; i>= 10; i++){
+//agregar valir al arreglo en la ultima posicion
+  testArray.push(i);
+}
+  console.log(testArray);
+  //eliminar primer valor
+  testArray.shift();
+  console.log(testArray);
+  //eliminar el último valor del arreglo
+  testArray.pop();
+  console.log(testArray);
+  //revertir los elementos del arreglo
+  testArray.reverse();
+  console.log(testArray);
+  //***************
+
+  var testString = ("abcd");
+  charAt.testString(2);
+  //para pedir un elemento desde su indice peeeeero porque ya tengo declarada mi strings
+
+  /*si no tengo declarada mi string uso el código ascii que tiene una
+  convinacion de numeros con la que llama cada letra del abecedario ya se
+  mayuscula o minuscula*/
+
+  // usamos el método sting.CharcodeAt(); cuando queremos saber el codigo ascii del elemento de un string
+  // pero OJOOOOOOO debe estar el string declarado
+  //String.fromCharCodeAt(nuemor ascci)---> devuelve la letra o elemento
+  //"elemento".charCodeAt()//--> devuelve codigo ascci
+
+  
